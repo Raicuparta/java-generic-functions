e6e0231a08c0707dcb74c5f0bcd49bbd94bc8b7c
Raicuparta <raicuparta@gmail.com>
2016-05-05 15:41:34 +0100

    separate main

--- src/ist/meic/pa/GenericFunctions/GenericFunction.java
+++ src/ist/meic/pa/GenericFunctions/GenericFunction.java
@@ -1,8 +1,6 @@
 package ist.meic.pa.GenericFunctions;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.lang.reflect.*;
 
 public class GenericFunction {
@@ -25,7 +23,6 @@ public class GenericFunction {
 
 	public ArrayList<GFMethod> sort(ArrayList<GFMethod> applicables, boolean specificFirst) {
 		ArrayList<GFMethod> sorted = new ArrayList<GFMethod>();
-
 		for (GFMethod appGfm : applicables) {
 			if (sorted.isEmpty()) {
 				sorted.add(appGfm);
@@ -62,7 +59,6 @@ public class GenericFunction {
 		return sorted;
 	}
 
-	// TODO tentar ordenar ao mesmo tempo se calhar
 	public ArrayList<GFMethod> getApplicableMethods(ArrayList<GFMethod> methods, Object[] args) {
 		ArrayList<GFMethod> applicables = new ArrayList<GFMethod>();
 		for (GFMethod gfm : methods) {
@@ -133,49 +129,5 @@ public class GenericFunction {
 			}
 		}
 		return null;
-	}
-
-	public static void println(Object obj) {
-		if (obj instanceof Object[]) {
-			System.out.println(Arrays.deepToString((Object[]) obj));
-		} else {
-			System.out.println(obj);
-		}
-	}
-
-	public static void main(String[] args) throws Throwable {
-		final GenericFunction explain = new GenericFunction("explain");
-		explain.addMethod(new GFMethod() {
-			Object call(Integer entity) {
-				System.out.printf("%s is a integer", entity);
-				return "";
-			}
-		});
-		explain.addMethod(new GFMethod() {
-			Object call(Number entity) {
-				System.out.printf("%s is a number", entity);
-				return "";
-			}
-		});
-		explain.addMethod(new GFMethod() {
-			Object call(String entity) {
-				System.out.printf("%s is a string", entity);
-				return "";
-			}
-		});
-		explain.addAfterMethod(new GFMethod() {
-			void call(Integer entity) {
-				System.out.printf(" (in hexadecimal, is %x)", entity);
-			}
-		});
-		explain.addBeforeMethod(new GFMethod() {
-			void call(Number entity) {
-				System.out.printf("The number ", entity);
-			}
-		});
-		println(explain.call(123));
-		println(explain.call("Hi"));
-		println(explain.call(3.14159));
-
 	}
 }
